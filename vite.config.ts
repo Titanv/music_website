a09175f6137db04447586a54ed3724a7bba4f573
Titan Vision <139152193+Titanv@users.noreply.github.com>
2025-06-25 23:46:45 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: "/", // e.g., "/music_website/"
+  base: '/', // 👈 necessary for custom domain
   plugins: [react()],
 });
