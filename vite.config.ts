b23109b5a8a2461d1f21fb525f36d4f2da2d99f3
“Titan <aflower3.1415@gmail.com>
2025-06-28 19:22:33 -0400

    update for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+    },
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
