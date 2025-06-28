438889b5e54562bfe64accfb2005786a6d5f422c
“Titan <aflower3.1415@gmail.com>
2025-06-28 18:06:16 -0400

    update for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  //other cofiguration..
+  base: '/',
   plugins: [react()],
-  base: "/",
 })
